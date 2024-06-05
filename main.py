cf0c2b93985453928b801b0201059cc5f126647e
Vasilev Dmitry <mekon26@gmail.com>
2024-06-05 14:38:41 +0300

    Access-Control-Allow-Origin fix

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ from fastapi.responses import JSONResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.staticfiles import StaticFiles
 from langchain_community.chat_models import GigaChat
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 security = HTTPBasic()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
